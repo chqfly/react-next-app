c0fea65ab47a778bc1fbe0200685e00f8c35778e
caihongquan <caihongquan@medlinker.com>
2019-08-20 17:05:43 +0800

    feat: 添加ant-design-mobile组件库 并解决antd样式被css modules编译的问题

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
+const withCss = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
+const cssLoaderGetLocalIdent = require("css-loader/lib/getLocalIdent.js");
 const path = require('path');
 
-module.exports = withSass({
+module.exports = withCss(withSass({
   cssModules: true,
+  cssLoaderOptions: {
+    getLocalIdent: function(context, localIdentName, localName, options) {
+      const hz = context.resourcePath.replace(context.rootContext, "");
+      if (/node_modules/.test(hz)) {
+        return localName;
+      } else {
+        return cssLoaderGetLocalIdent(context, localIdentName, localName, options); 
+      }
+    }
+  },
   webpack(config, options) {
     config.resolve.alias = Object.assign(config.resolve.alias, {
       '@': path.resolve(__dirname, './'),
     });
     return config;
-  }
-});
+  },
+}));
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
-const Index: React.SFC = () => <div>index</div>;
+import { Button } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
+const Index: React.SFC = () => (
+  <div>
+    <Button type="primary">按钮</Button>
+  </div>
+);
 
 export default Index;
